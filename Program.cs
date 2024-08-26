c833dcfc0cbd6ee8bd8d0016217b1073e05c5a92
Nick A <nakursk@gmail.com>
2024-08-26 17:00:42 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SF.HW03.Task13_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // читаем весь файл с рабочего стола в строку текста
+            var filePath = Path.Combine(DirectoryExtension.GetSolutionRoot(), "Text12.txt");
+            var text = string.Empty;
+            try
+            {
+                text = File.ReadAllText(filePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+            // Сохраняем символы-разделители в массив
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+            // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            // выводим количество
+            Console.WriteLine(words.Length);
+        }
+    }
+}
